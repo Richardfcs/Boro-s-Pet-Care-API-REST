e6c4e0f810f18839297787c7367d39f25869c596
Richardfcs <richardfernandodecastrodasilva@gmail.com>
2025-03-11 23:54:00 -0300

    Atualização 10

--- functions/api/server.js
+++ functions/api/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import express from 'express';
+// import serverless from "serverless-http";
 
 import { routes } from './routes.js';
 
